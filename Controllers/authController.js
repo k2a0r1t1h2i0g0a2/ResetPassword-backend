c96a3fb8bd7551ade1503a4654ad7dda7ded11af
k2a0r1t1h2i0g0a2 <karthiga.r2002@gmail.com>
2023-12-08 11:49:55 +0530

    url changes

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -44,7 +44,7 @@ export const forgotPassword = async (req, res) => {
     user.resetPasswordExpires = Date.now() + 3600000;
     await user.save();
 
-    const resetLink = `${process.env.BASEURL}/reset-password/${resetToken}`;
+    const resetLink = `${process.env.BASEURL}reset-password/${resetToken}`;
     console.log("BASEURL:", process.env.BASEURL);
  console.log("Reset Link:", resetLink);
 
